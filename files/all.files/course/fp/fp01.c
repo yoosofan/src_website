9b2ea29a9140836bdbc7aa60786240790b294464
Ahmad Yoosofan <yoosofan@myfastmail.com>
2015-08-13 23:07:25 +0430

    files/ is added

---
+++ files/all.files/course/fp/fp01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  printf("**********");
+  printf("**********");
+  printf("**********");
+  printf("**********");
+  printf("**********");
+  printf("**********");
+  printf("**********");
+  return 0;
+}
