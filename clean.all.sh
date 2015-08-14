29d2901857ebaab0e3a3d9027969636ae4176ead
Ahmad Yoosofan <yoosofan@myfastmail.com>
2015-08-14 17:51:17 +0430

    clean.all.sh remove all *.*~ fixed

--- clean.all.sh
+++ clean.all.sh
@@ -2,5 +2,7 @@ nikola clean
 nikola check --clean-files
 rm output/ cache/ __pycache__/ -rf
 rm .doit.db
-rm *~ -r
+#http://unix.stackexchange.com/questions/116389/recursively-delete-all-files-with-a-given-extension
+#http://askubuntu.com/questions/377438/how-can-i-recursively-delete-all-files-of-a-specific-extension-in-the-current-di
+find . -type f -name '*.*~' -delete
 #nikola build
