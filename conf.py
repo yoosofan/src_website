1d2afb27a7ad133f43250c5032891175f29eea89
Ahmad Yoosofan <yoosofan@myfastmail.com>
2015-08-15 15:08:09 +0430

    Add README to output

--- conf.py
+++ conf.py
@@ -20,7 +20,8 @@ BLOG_AUTHOR = { "fa": "احمد یوسفان" , "en": "Ahmad Yoosofan",}
 BLOG_TITLE ={ "fa": "احمد یوسفان" ,   "en": "Ahmad Yoosofan", }
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://yoosofan.kashanu.ac.ir/"
+#SITE_URL = "http://yoosofan.github.io/yoosofan/"
+SITE_URL = "http://yoosofan.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
@@ -363,11 +364,11 @@ REDIRECTIONS = []
 
 # For user.github.io OR organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
-# GITHUB_SOURCE_BRANCH = 'master'
-# GITHUB_DEPLOY_BRANCH = 'gh-pages'
+GITHUB_SOURCE_BRANCH = 'source'
+GITHUB_DEPLOY_BRANCH = 'master'
 
 # The name of the remote where you wish to push to, using github_deploy.
-# GITHUB_REMOTE_NAME = 'origin'
+#GITHUB_REMOTE_NAME = 'origin'
 
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
