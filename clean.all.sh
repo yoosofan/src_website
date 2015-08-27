e10d4b4c4a789be0a46a0bb96bf86e8b51024c06
Ahmad Yoosofan <yoosofan@myfastmail.com>
2015-08-27 18:18:22 +0430

    compiler changed

--- clean.all.sh
+++ clean.all.sh
@@ -4,5 +4,7 @@ rm output/ cache/ __pycache__/ -rf
 rm .doit.db
 #http://unix.stackexchange.com/questions/116389/recursively-delete-all-files-with-a-given-extension
 #http://askubuntu.com/questions/377438/how-can-i-recursively-delete-all-files-of-a-specific-extension-in-the-current-di
+cd ..
+rm output__* -rf
 find . -type f -name '*.*~' -delete
 #nikola build
