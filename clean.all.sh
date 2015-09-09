8b4756e49342938e486091fd69b36c9708d4b927
Ahmad Yoosofan <yoosofan@myfastmail.com>
2015-09-10 03:08:54 +0430

    version Thu Sep 10 03:08:54 IRDT 2015

--- clean.all.sh
+++ clean.all.sh
@@ -2,6 +2,7 @@ nikola clean
 nikola check --clean-files
 rm output/ cache/ __pycache__/ -rf
 rm .doit.db
+rm *.pyc
 #http://unix.stackexchange.com/questions/116389/recursively-delete-all-files-with-a-given-extension
 #http://askubuntu.com/questions/377438/how-can-i-recursively-delete-all-files-of-a-specific-extension-in-the-current-di
 cd ..
