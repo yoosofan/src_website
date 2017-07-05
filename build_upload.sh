35eeddcaf5f8f6f714d51fa8d73a045e253a5c48
Ahmad Yoosofan <yoosofan@fastmail.fm>
2017-07-05 11:59:29 +0430

    version Wed Jul  5 11:59:29 +0430 2017

--- build_upload.sh
+++ build_upload.sh
@@ -2,11 +2,11 @@
 
 nikola build --quiet
 echo 'After build for kashanu'
-rsync -av --delete --exclude=".*" output/ ../output__kashanu/
+rsync -av --delete --exclude=".*" output/ ../output__kashanu/  --quiet
 python3 run.before.build.py
 nikola build --quiet
 echo 'After build for github'
-rsync -av --delete --exclude=".*" output/ ../output__github/
+rsync -av --delete --exclude=".*" output/ ../output__github/  --quiet
 python3 run.before.build.py
 
 sh clean.all.sh
